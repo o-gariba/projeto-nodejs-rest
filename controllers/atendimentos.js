17f6b8d54b00893f5515df13d254e4924a098e7e
Pedro Garibaldi Santos <pedrogaribaldisantos@gmail.com>
2022-03-15 10:49:51 -0300

    Lendo Json no post.

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -3,5 +3,8 @@ module.exports = app => {
         res.send('Você está em ATENDIMENTOS e está realizando um GET.')
     })
 
-    app.post('/atendimentos', (req, res) => res.send('Vc está na rota de atendimentos e está realizando um POST'))
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('Vc está na rota de atendimentos e está realizando um POST')
+    })
 }
