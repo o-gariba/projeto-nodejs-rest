c86e16f6bdcca4993346455a512472d49302c371
Pedro Garibaldi Santos <pedrogaribaldisantos@gmail.com>
2022-03-16 15:26:19 -0300

    Adicionada a tabela data, moment add, status http.

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -8,8 +8,6 @@ module.exports = app => {
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body
 
-        Atendimento.adiciona(atendimento)
-        
-        res.send('Atendimento enviado!')
+        Atendimento.adiciona(atendimento, res)
     })
 }
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,12 +1,18 @@
 const conexao = require('../infra/conexao')
+const moment = require('moment')
 
 class Atendimento {
-    adiciona(atendimento) {
+    adiciona(atendimento, res) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+
+        const atendimentoDatado = {...atendimento, dataCriacao, data}
+
         const sql = 'INSERT INTO atendimentos SET ?'
 
-        conexao.query(sql, atendimento, (erro, resultado) => {
-            if (erro) throw erro
-            else console.log(resultado)
+        conexao.query(sql, atendimentoDatado, (erro, resultado) => {
+            if (erro) res.status(400).json(erro)
+            else res.status(201).json(resultado)
         })
     }
 }
