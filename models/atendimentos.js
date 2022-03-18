87ec0740bd944773d08047a5649eca802216bdd1
Pedro Garibaldi Santos <pedrogaribaldisantos@gmail.com>
2022-03-18 15:50:10 -0300

    Feedback semantico rotas finalizadas.

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -32,7 +32,7 @@ class Atendimento {
 
             conexao.query(sql, atendimentoDatado, (erro, resultado) => {
                 if (erro) res.status(400).json(erro)
-                else res.status(201).json(resultado)
+                else res.status(201).json(atendimento)
             })
         }
 
@@ -55,6 +55,25 @@ class Atendimento {
 
             if (erro) res.status(400).json(erro)
             else res.status(200).json(atendimento)
+        })
+    }
+
+    altera(id, valores, res) {
+        if (valores.data) valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
+        const sql = 'UPDATE atendimentos SET ? WHERE id = ?'
+
+        conexao.query(sql, [valores, id], (erro, resultados) => {
+            if (erro) res.status(400).json(erro)
+            else res.status(200).json(resultados)
+        })
+    }
+
+    deleta(id, res) {
+        const sql = 'DELETE FROM atendimentos WHERE id = ?'
+
+        conexao.query(sql, id, (erro, resultados) => {
+            if (erro) res.status(400).json(erro)
+            else res.status(200).json({...valores, id})
         })
     }
 }
