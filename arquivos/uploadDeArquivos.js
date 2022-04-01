a9d142f57425624fd051b5d3a5ac59c2a58b93fb
Pedro Garibaldi Santos <pedrogaribaldisantos@gmail.com>
2022-04-01 12:20:31 -0300

    Usando streams para copiar uma imagem

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,12 +1,16 @@
  const fs = require('fs')
 
- fs.readFile('../assets/salsicha.jpg', (erro, buffer) => {
-    if (erro) throw new Error(erro)
-    else {
-        console.log('A imagem foi bufferizada')
-        const imgBufferizada = buffer
-        console.log(imgBufferizada)
-                                                       //outro erro, outro escopo 
-        fs.writeFile('../assets/salsicha2.jpg', buffer, erro => console.log('A imagem foi escrita com sucesso!'))
-    }
- })
+//  fs.readFile('../assets/salsicha.jpg', (erro, buffer) => {
+//     if (erro) throw new Error(erro)
+//     else {
+//         console.log('A imagem foi bufferizada')
+//         const imgBufferizada = buffer
+//         console.log(imgBufferizada)
+//                                                        //outro erro, outro escopo 
+//         fs.writeFile('../assets/salsicha2.jpg', buffer, erro => console.log('A imagem foi escrita com sucesso!'))
+//     }
+//  })
+
+fs.createReadStream('../assets/salsicha.jpg')
+    .pipe(fs.createWriteStream('../assets/salsicha-stream.jpg'))
+    .on('finish', () => console.log('Imagem escrita através de streams'));
